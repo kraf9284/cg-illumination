e345b4c30bc7a1ad9cc439dd2bf622870cd1c93c
Jonny DeMarais <dema5892@stthomas.edu>
2024-05-18 23:22:20 -0500

    Editing README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 3D Rendering - Illumination and Shading
 3D illumination &amp; shading starter code using the Babylon and WebGL APIs
+
+<h3> Setup </h3>
+
+To run this code:
+
+<ol>
+    <li>Download the folder containing the code for this project</li>
+    <li>Run the project in a software and open a new terminal</li>
+    <li>Type npm run dev -- --port 8000 and follow the link in the terminal</li>
+</ol>
+
+<h3> About </h3>
+
+This project contains three 3 dimensional interactive scenes. At the top of the page you will have the option to change the scene, 
+the shading algorithm, heightmap scale, and primary light source. You can use the arrow keys to move around and wasdrf to change the 
+the position of the light.
--- src/renderer.js
+++ src/renderer.js
@@ -434,7 +434,7 @@ class Renderer {
             mat_shininess: 16,
             texture_scale: new Vector2(1.0, 1.0)
         }
-        cust.material = materials['illum_' + this.shading_alg];
+        //cust.material = materials['illum_' + this.shading_alg];
         //current_scene.models.push(cust);
 
         // Animation function - called before each cust gets rendered
@@ -474,6 +474,126 @@ class Renderer {
             this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
         });
     }
+    createScene3(scene_idx) {
+        let current_scene = this.scenes[scene_idx];
+        let scene = current_scene.scene;
+        let materials = current_scene.materials;
+        let ground_mesh = current_scene.ground_mesh;
+
+        // Set scene-wide / environment values
+        scene.clearColor = current_scene.background_color;
+        scene.ambientColor = current_scene.ambient;
+        scene.useRightHandedSystem = true;
+
+        // Create camera
+        current_scene.camera = new UniversalCamera('camera', new Vector3(0.0, 1.8, 10.0), scene);
+        current_scene.camera.setTarget(new Vector3(0.0, 1.8, 0.0));
+        current_scene.camera.upVector = new Vector3(0.0, 1.0, 0.0);
+        current_scene.camera.attachControl(this.canvas, true);
+        current_scene.camera.fov = 35.0 * (Math.PI / 180);
+        current_scene.camera.minZ = 0.1;
+        current_scene.camera.maxZ = 100.0;
+
+        // Create light0 - White Light
+        let light0 = new PointLight('light0', new Vector3(1.0, 5.0, 5.0), scene);
+        light0.diffuse = new Color3(1.0, 1.0, 1.0); // White light
+        light0.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light0);
+
+        let turf_texture = new Texture(BASE_URL + 'textures/field.jpg', scene);
+
+        // Create ground mesh
+        let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
+        let ground_heightmap = new Texture(BASE_URL + 'heightmaps/mulch-heightmap.png', scene);
+        ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
+        ground_mesh.metadata = {
+            mat_color: new Color3(1, 1, 1),
+            mat_texture: turf_texture,
+            mat_specular: Color3.Black(),
+            mat_shininess: 1,
+            texture_scale: new Vector2(1.0, 1.0),
+            height_scalar: 1.0,
+            heightmap: ground_heightmap
+        }
+
+        ground_mesh.material = materials['ground_' + this.shading_alg];
+        
+            // Create stacked spheres
+        let sphere1 = CreateSphere('sphere1', { segments: 32, diameter: 1 }, scene);
+        sphere1.position = new Vector3(0, 0.5, 0); // Position at the center
+        sphere1.metadata = {
+            mat_color: new Color3(1, 1, 1), // Red
+            mat_texture: new Texture(BASE_URL + 'textures/balldimpled.jpg', scene),
+            mat_specular: new Color3(0.2, 0.2, 0.2),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        sphere1.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(sphere1);
+
+        let sphere2 = CreateSphere('sphere2', { segments: 32, diameter: 1 }, scene);
+        sphere2.position = new Vector3(0, 1.5, 0); // Position above the first sphere
+        sphere2.metadata = {
+            mat_color: new Color3(1, 1, 1), // Green
+            mat_texture: new Texture(BASE_URL + 'textures/baseball.jpg', scene),
+            mat_specular: new Color3(0.1, 0.1, 0.1),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        sphere2.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(sphere2);
+
+        let sphere3 = CreateSphere('sphere3', { segments: 32, diameter: 1 }, scene);
+        sphere3.position = new Vector3(0, 2.5, 0); // Position above the second sphere
+        sphere3.metadata = {
+            mat_color: new Color3(1, 1, 1), // Blue
+            mat_texture: new Texture(BASE_URL + 'textures/futbol.jpg', scene),
+            mat_specular: new Color3(0.8, 0.8, 0.8),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        //cust.material = materials['illum_' + this.shading_alg];
+        //current_scene.models.push(cust);
+
+        // Animation function - called before each cust gets rendered
+        sphere3.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(sphere3);
+
+
+        scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.event.key) {
+                case "a":
+                    current_scene.lights[this.active_light].position.x -= 1;
+                    break;
+                case "d":
+                    current_scene.lights[this.active_light].position.x += 1;
+                    break;
+                case "f":
+                    current_scene.lights[this.active_light].position.y -= 1;
+                    break;
+                case "r":
+                    current_scene.lights[this.active_light].position.y += 1;
+                    break;
+                case "w":
+                    current_scene.lights[this.active_light].position.z -= 1;
+                    break;
+                case "s":
+                    current_scene.lights[this.active_light].position.z += 1;
+                    break;
+            }  
+        });
+        // Animation function - called before each frame gets rendered
+        scene.onBeforeRenderObservable.add(() => {
+            // update models and lights here (if needed)
+            // ...
+
+            // update uniforms in shader programs
+            this.updateShaderUniforms(scene_idx, materials['illum_' + this.shading_alg]);
+            this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
+        });
+    }
+
+    
 
     updateShaderUniforms(scene_idx, shader) {
         let current_scene = this.scenes[scene_idx];
